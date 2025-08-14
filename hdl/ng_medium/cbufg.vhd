b37b7e29569d018dd900028b18c0e02521c68395
deuskane <deuskane@gmail.com>
2025-08-14 07:18:32 +0200

    Use WFG for cbufg into ng_medium

--- hdl/ng_medium/cbufg.vhd
+++ hdl/ng_medium/cbufg.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosière
 -- Company    : 
 -- Created    : 2025-08-11
--- Last update: 2025-08-12
+-- Last update: 2025-08-14
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -36,13 +36,37 @@ end cbufg;
 architecture rtl of cbufg is
 begin
 
-  ins_nx_bd : NX_BD
-    generic map (
-      mode => "global_lowskew"
-      )
-    port    map (
-      I	   => d_i,
-      O	   => d_o
-      );
+  -- ins_clock_buffer : NX_BD
+  --   generic map (
+  --     mode => "global_lowskew"
+  --     )
+  --   port    map (
+  --     I    => d_i,
+  --     O    => d_o
+  --     );
+
+  -- ins_clock_buffer : NX_CSC
+  --   port    map (
+  --     I    => d_i,
+  --     O    => d_o
+  --     );
+
+  ins_clock_buffer :  NX_WFG
+    generic map(
+        location     => "",
+        wfg_edge     => '0',             -- 0: no invert / Rising
+        mode         => '0',             -- 0: no pattern - 1: pattern
+        pattern_end  => 0,               -- 0: to 15 (1 step to 16 steps)                 
+        pattern      => (others => '0'), -- pattern p0 ... p15
+        delay_on     => '0',             -- 0: no delay - 1: delay 
+        delay        => 0                -- 0 to 63 (1 unit to 64 unit)
+    )
+    port map(
+        ZI           => d_i,
+        ZO           => d_o,
+        SI           => '0',
+        SO           => open,
+        RDY          => '1'
+    );
   
 end rtl;
