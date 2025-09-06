0814445cb7eeb1627714a010ec4a7f6f5bca5a85
deuskane <deuskane@gmail.com>
2025-09-06 14:48:21 +0200

    update package in tb

--- sim/src/tb_dummy.vhd
+++ sim/src/tb_dummy.vhd
@@ -6,7 +6,7 @@
 -- Author     : Mathieu Rosière
 -- Company    : 
 -- Created    : 2025-01-06
--- Last update: 2025-08-11
+-- Last update: 2025-09-06
 -- Platform   : 
 -- Standard   : VHDL'87
 -------------------------------------------------------------------------------
@@ -20,8 +20,10 @@
 -------------------------------------------------------------------------------
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
+use     IEEE.STD_LOGIC_1164.ALL;
+use     IEEE.numeric_std.ALL;
+library work;
+use     work.techmap_pkg.all;
 
 entity tb_dummy is
 end tb_dummy;
@@ -29,13 +31,13 @@ end tb_dummy;
 architecture rtl of tb_dummy is
 begin
   
-  cbufg : entity work.cbufg(rtl)
+  ins_cbufg : cbufg
   port map (
     d_i   => '0',
     d_o   => open
     );
   
-  cgate : entity work.cgate(rtl)
+  ins_cgate : cgate
   port map (
     clk_i    => '0',
     cke_i    => '0',
@@ -43,14 +45,14 @@ begin
     clk_o    => open
     );
 
-  sync2dff : entity work.sync2dff(rtl)
+  ins_sync2dff : sync2dff
   port map (
     clk_i => '0',
     d_i   => '0',
     q_o   => open
     );
 
-  sync2dffrn : entity work.sync2dffrn(rtl)
+  ins_sync2dffrn : sync2dffrn
   port map (
     clk_i    => '0',
     arst_b_i => '0',
